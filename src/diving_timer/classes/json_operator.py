6a1000a6887467febbf5e2324f8eabf710d545dd
isarenko.dmitry <isarenko.dmitry.it@gmail.com>
2024-09-26 22:22:50 +0300

    started creating json structure

---
+++ src/diving_timer/classes/json_operator.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+
+class JsonOperator:
+    def __init__(self):
+        pass
+
+    def find_json(self, *, filepath: str) -> list[json]:
+        files_list = os.listdir()
+        for file in files_list:
+            if type(file) is json:
+                pass
+        return []
+
+    def read_json(self, *, filepath: str) -> dict:
+        pass
+
+    def write_json(self, *, filepath: str) -> bool:
+        pass
+
+    def send_json_to_db(self, *, filepath: str, db) -> bool:
+        pass
+
+    def get_json_from_db(self, *, filepath: str, db) -> bool:
+        pass
