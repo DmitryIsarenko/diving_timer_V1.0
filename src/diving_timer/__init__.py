bd25bd65898d4f9d8908aa9b369e3e585c3df734
isarenko.dmitry <isarenko.dmitry.it@gmail.com>
2024-09-29 15:57:25 +0300

    update

--- src/diving_timer/__init__.py
+++ src/diving_timer/__init__.py
@@ -3,12 +3,12 @@ import logging
 logger = logging.getLogger("src")
 logger.setLevel(logging.DEBUG)
 
-format_string = "%(levelname)-8s %(name)s %(lineno)3d %(asctime)s  %(message)s"
-formatter = logging.Formatter(format_string)
+format_string = "%(asctime)s %(levelname)-8s --- %(message)s --- %(name)s:%(lineno)d"
+formatter = logging.Formatter(format_string, datefmt="%Y-%m-%d %H:%M:%S")
 
-handler = logging.FileHandler(filename="./logs/master.log",mode="w")
+handler = logging.FileHandler(filename="../logs/log.log",mode="w")
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
-logger.warning(f"Message 1")
+logger.warning(f"Message!")
